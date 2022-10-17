9c1a45d99514dfe7b8923e981956cc18f7bfe15a
mironVY <mw.kld.rus@gmail.com>
2022-10-17 23:47:17 +0200

    первый трай

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import jwtDecode from 'jwt-decode'
+
+import store from './store'
+
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import NotFound from './components/not-found/NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Provider store={store}>
+        <BrowserRouter>
+          <React.Fragment>
+              <Header />
+              <div className="container">
+                  <Route path="/404" component={NotFound} />
+              </div>
+              <Footer />
+          </React.Fragment>
+        </BrowserRouter>
+      </Provider>
+  )
 }
 
 export default App;
---
+++ src/components/not-found/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default () => {
+    <div className="text-center mt-5">
+        <h1>Page not found</h1>
+    </div>
+}
